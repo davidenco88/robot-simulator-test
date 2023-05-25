8ea90826e0c294d1937bbe63230be5e586887547
David Sarria <david.sarriav@gmail.com>
2023-05-25 15:55:53 +0200

    Feat: Add exercise solution

--- robot.js
+++ robot.js
@@ -1,28 +1,137 @@
 function createRobot(coordinates, direction) {
-  let [x, y] = coordinates
-  let orientation = direction 
+
+  let [x, y] = [0, 0];
+  let orientation = '';
+
+  if (x > 10 || x < 0 || y > 10 || y < 0) {
+    throw new Error('Invalid cordinate - outside of area');
+  } else {
+    [x, y] = coordinates
+  }
+
+  const directions = ['North', 'South', 'East', 'West'];
+  const validDirection = directions.includes(direction);
+
+  if (!validDirection) {
+    throw new Error('Invalid cordinate - outside of area');
+  } else {
+    orientation = direction
+  }
 
   return {
     getPosition: () => {
+
       return { coordinates: [x, y], orientation }
     },
-    advance: () => {
-      // función para avanzar
-    },
-    turnRight: () => {
-      // función para girar a la derecha
-    },
-    turnLeft: () => {
-      // función para girar a la izquierda
-    },
-    instructions: (stringInstructions) => {
-      // función para recibir una serie de instrucciones Ejm: "RAALAL"
-      // En este metodo se va crear 
+
+    advance: function () {
+      //función para avanzar
+      let new_y = 0;
+      let new_x = 0;
+
+      switch (orientation) {
+        case 'North':
+          new_y = y + 1;
+          if (new_y > 10 || new_y < 0) {
+            throw new Error('Invalid movement - outside of area');
+          } else {
+            y = new_y
+          }
+          break;
+
+        case 'South':
+          new_y = y - 1;
+          if (new_y > 10 || new_y < 0) {
+            throw new Error('Invalid movement - outside of area');
+          } else {
+            y = new_y
+          }
+          break;
+
+        case 'East':
+          new_x = x + 1;
+          if (new_x > 10 || new_x < 0) {
+            throw new Error('Invalid movement - outside of area');
+          } else {
+            x = new_x
+          }
+          break;
+
+        case 'West':
+          new_x = x - 1;
+          if (new_x > 10 || new_x < 0) {
+            throw new Error('Invalid movement - outside of area');
+          } else {
+            x = new_x
+          }
+          break;
+        }
+      },
+
+      turnRight: function () {
+        // función para girar a la derecha
+        switch (orientation) {
+          case 'North':
+            orientation = 'East';
+            break;
+
+          case 'South':
+            orientation = 'West';
+            break;
+
+          case 'East':
+            orientation = 'South';
+            break;
+
+          case 'West':
+            orientation = 'North';
+            break;
+        }
+      },
+
+      turnLeft: function () {
+        // función para girar a la izquierda
+
+        switch (orientation) {
+          case 'North':
+            orientation = 'West';
+            break;
+
+          case 'South':
+            orientation = 'East';
+            break;
+
+          case 'East':
+            orientation = 'North';
+            break;
+
+          case 'West':
+            orientation = 'South';
+            break;
+        }
+      },
+
+      instructions: function (stringInstructions) {
+        // función para recibir una serie de instrucciones Ejm: "RAALAL"
+        // En este metodo se va crear
+        const arrayInstructions = [...stringInstructions];
+        arrayInstructions.map((instruction) => {
+          switch (instruction) {
+            case 'A':
+              this.advance();
+              break;
+            case 'R':
+              this.turnRight();
+              break;
+            case 'L':
+              this.turnLeft();
+              break;
+            default:
+              throw new Error('Invalid Instruction, there are just three valid instructions -A-, -R-, -L-');
+          }
+        })
+      }
     }
   }
-}
 
-module.exports = createRobot
-
-
-console.log(createRobot([2,3], "North").getPosition());
+  module.exports = { createRobot }
--- robot.test.js
+++ robot.test.js
@@ -1,7 +1,63 @@
 const { createRobot } = require('./robot.js');
 
-describe('Robot', () => {
-	test('Cambia el titulo y empieza a escribir tu primera prueba', () => {
+describe('createRobot', () => {
+  test('should return the correct initial position and orientation', () => {
+    const robot = createRobot([0, 0], 'North');
+    const position = robot.getPosition();
+    expect(position.coordinates).toEqual([0, 0]);
+    expect(position.orientation).toBe('North');
+  });
 
-	});
+  test('should advance the robot correctly', () => {
+    const robot = createRobot([0, 0], 'North');
+
+    robot.advance();
+    let position = robot.getPosition();
+    expect(position.coordinates).toEqual([0, 1]);
+    expect(position.orientation).toBe('North');
+
+    robot.turnRight();
+    robot.advance();
+    position = robot.getPosition();
+    expect(position.coordinates).toEqual([1, 1]);
+    expect(position.orientation).toBe('East');
+  });
+
+  test('should throw an error when advancing outside the area', () => {
+    const robot = createRobot([0, 10], 'North');
+    expect(() => { robot.advance() }).toThrow();
+  });
+
+  test('should turn right correctly', () => {
+    const robot = createRobot([0, 0], 'North');
+
+    robot.turnRight();
+    let position = robot.getPosition();
+    expect(position.orientation).toBe('East');
+
+    robot.turnRight();
+    position = robot.getPosition();
+    expect(position.orientation).toBe('South');
+  });
+
+  test('should turn left correctly', () => {
+    const robot = createRobot([0, 0], 'North');
+
+    robot.turnLeft();
+    let position = robot.getPosition();
+    expect(position.orientation).toBe('West');
+
+    robot.turnLeft();
+    position = robot.getPosition();
+    expect(position.orientation).toBe('South');
+  });
+
+  test('should execute instructions correctly', () => {
+    const robot = createRobot([0, 0], 'North');
+
+    robot.instructions('RAALAL');
+    const position = robot.getPosition();
+    expect(position.coordinates).toEqual([2, 1]);
+    expect(position.orientation).toBe('West');
+  });
 });
